26c45706234bc8c52d7f90f4d30e4e48bd7484b3
oscar <oscar.burman005@gmail.com>
2025-02-06 10:49:32 +0100

    Created primary, secondary, danger

---
+++ src/components/btn/SecondaryBtn.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function SecondaryBtn({
+  type = "button",
+  btnText,
+  fontSize = "base",
+  width = "w-full",
+  onClick,
+  icon,
+}) {
+  const handleClick = () => {
+    if (onClick) onClick();
+  };
+
+  return (
+    <button
+      type={type}
+      onClick={handleClick}
+      onMouseLeave={(e) => e.target.blur()}
+      className={`flex justify-center items-center ${width} h-fit text-white font-semibold py-[10px] px-[25px] rounded-full transition-opacity duration-500 cursor-pointer group bg-blue-600
+ hover:opacity-60`}>
+      {icon && (
+        <div className="text-white mr-2 transition-transform duration-300 group-hover:translate-x-[5px]">
+          {icon}
+        </div>
+      )}
+      <p className={`text-${fontSize} whitespace-nowrap`}>{btnText}</p>
+    </button>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import PrimaryBtn from "../components/btn/PrimaryBtn";
+import SecondaryBtn from "../components/btn/SecondaryBtn";
+import DangerBtn from "../components/btn/DangerBtn";
+import { IoMdCheckmark } from "react-icons/io";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { IoArrowForwardSharp } from "react-icons/io5";
 
 export default function HomePage() {
   return (
@@ -9,6 +15,11 @@ export default function HomePage() {
         <li>Schema</li>
         <li>Bigbigboymanboy</li>
       </ul>
+      <div className="space-y-2 p-6">
+        <PrimaryBtn btnText="Primary" icon={<IoMdCheckmark size={24} />}></PrimaryBtn>
+        <SecondaryBtn btnText="Secondary" icon={<IoArrowForwardSharp size={24} />} />
+        <DangerBtn btnText="Danger" icon={<FaRegTrashCan size={24} />} />
+      </div>
     </div>
   );
 }
