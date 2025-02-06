0d732f45d79898cbee9b08a43739fd9ce64cc7de
oscar <oscar.burman005@gmail.com>
2025-02-06 12:04:06 +0100

    Created a custom hook for handling account management

---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+export const DB_URL = "htttp://localhost:5000";
+export const DB_USERS_ENDPOINT = `${DB_URL}/users`;
---
+++ src/hooks/useUsersDB.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { DB_USERS_ENDPOINT } from "../constants/constants";
+
+const useUsersDB = () => {
+  //user have props username, email and password
+  const signUp = async (user) => {
+    try {
+      const response = await axios.post(DB_USERS_ENDPOINT, user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signIn = async (user) => {
+    try {
+      const isEmail = user.includes("@");
+
+      const response = await axios.get(DB_USERS_ENDPOINT, {
+        params: {
+          [isEmail ? "email" : "username"]: user,
+        },
+      });
+
+      const userData = response.data;
+
+      if (userData.length > 0) {
+        // User found, handle successful sign-in
+        console.log("User found:", userData);
+      } else {
+        // No user found
+        console.log("User not found");
+      }
+    } catch (error) {
+      console.error("Error during sign-in:", error);
+    }
+  };
+
+  return { signUp, signIn };
+};
+
+export default useUsersDB;
