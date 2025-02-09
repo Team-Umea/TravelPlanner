743b77146b18ad6fdf1160af7c52b25d4f857ad6
oscar <oscar.burman005@gmail.com>
2025-02-09 18:03:36 +0100

    Cleared main

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { RouterProvider } from "react-router";
-import router from "./router/Router";
-
-export default function App() {
-  return <RouterProvider router={router} />;
-}
--- src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export function generateID(array) {
-  if (array.length === 0) {
-    return 1;
-  } else {
-    return Math.max(...array.map((item) => item.id)) + 1;
-  }
-}
